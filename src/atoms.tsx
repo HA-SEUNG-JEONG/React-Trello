5417c1dbc6e1df3a3dbef6f98233be0f128476fd
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-12-28 15:01:08 +0900

    Reordering - DropResult,destination,source,splice

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,19 +1,6 @@
 import { atom, selector } from "recoil";
 
-//atoms에 하나의 데이터만을 저장
-export const minuteState = atom({
-  key: "minutes",
-  default: 0,
-});
-
-export const hourSelector = selector<number>({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minuteState);
-    return minutes / 60;
-  },
-  set: ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60;
-    set(minuteState, minutes);
-  },
+export const toDoState = atom({
+  key: "toDo",
+  default: ["a", "b", "c", "d", "e", "f"],
 });
